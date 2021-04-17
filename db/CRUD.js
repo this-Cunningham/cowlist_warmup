dfa043e47283bd5c6ccc77f6adad579a8eca245a
Chris_Cunningham <this.cunningham@gmail.com>
2021-04-17 14:13:50 -0400

    Setup home route

--- db/CRUD.js
+++
@@ -1,5 +0,0 @@
-const { Cow } = require('./index.js');
-
-module.exports.getCows = () => {
-  return Cow.find()
-}
--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const db = require('../db/index.js')
+const { Cow } = db;
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  Cow.find()
+    .then((data) => res.status(200).send(data))
+    .catch(err => res.status(400).send({ err }))
+});
 
 app.listen(port, (err) => {
   if (err) console.log('error connecting to server')
