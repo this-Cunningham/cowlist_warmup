f238a1f22cfb9a488f3d99419e90e58011f3a1ae
Chris_Cunningham <this.cunningham@gmail.com>
2021-04-17 14:30:52 -0400

    render cow to page

--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 const App = () => {
+  const [cows, setCows] = useState([]);
+
   useEffect(() => {
     axios.get('/cows')
-      .then(({ data }) => console.log(data))
+      .then(({ data }) => setCows(data))
       .catch(err => console.log( err ))
   }, [])
   return (
-    <div>Hey</div>
+    <div className="container">
+      {cows.map((cow) => (
+        <div className="cow" key={cow.name+cow.weight}>
+          <h2>
+            {cow.name}
+          </h2>
+          <p>Description: {cow.description}</p>
+          <p>Weight: {cow.weight} lbs</p>
+        </div>
+      ))}
+    </div>
   )
 };
 
