8a1038a6d9bff6b329f658a54ec235405865ba17
Chris_Cunningham <this.cunningham@gmail.com>
2021-04-17 13:29:46 -0400

    Setup packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'src'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
